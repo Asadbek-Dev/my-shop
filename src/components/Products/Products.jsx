21bca796a8039d77b762a8111267fb4516343f09
Asadbek-Dev <asadbek.dev1101@gmail.com>
2021-06-21 23:14:58 +0500

    added navbar and styled

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import {Grid} from '@material-ui/core';
 import Product from './Product/Product';
 
+import useStyles from './styles';
+
 
 const products=[
     {id:1,name:'Shoes',description:'Running shoes.',price:'$5',image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMWw4K-XBiDoEMQ3wbDpJ0HGSIWOzulhDpaA&usqp=CAU'},
@@ -9,8 +11,10 @@ const products=[
 ]
 
 const Products = () => {
+    const classes=useStyles();
     return( 
-        <main>
+        <main className={classes.content}>
+            <div className={classes.toolBar} />
             <Grid container justify='center' spacing={4}>
                 {products.map((product)=>(
                     <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
