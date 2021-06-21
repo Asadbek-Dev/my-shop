6e9eb6835774b09669a996db5aa5e665ebe4dea1
Asadbek-Dev <asadbek.dev1101@gmail.com>
2021-06-22 00:02:28 +0500

    added commerce.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { commerce } from './lib/commerce'
 import {Products, Navbar} from './components';
 
 const App = () => {
+    const [products, setProducts] = useState([]);
+
+    const fetchProducts=async () => {
+        const { data } = await commerce.products.list();
+
+        setProducts(data);
+    }
+
+    useEffect(() => {
+       fetchProducts();
+    }, [])
+
+    console.log(products);
+
     return (
         <div>
             <Navbar/>
