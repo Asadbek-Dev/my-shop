c1182c159a2fda13a89e71850e386c94f814464a
Asadbek-Dev <asadbek.dev1101@gmail.com>
2021-06-24 21:25:59 +0500

    Added Checkout page

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -39,6 +39,7 @@ const Cart = ({ cart,handleUpdateCartQty,handleRemoveFromCart,handleEmptyCart })
               Empty Cart
             </Button>
             <Button
+            component={Link} to='/checkout'
               className={classes.checkoutButton}
               size="large"
               type="button"
---
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import { Paper, Stepper, Step, StepLabel, Typography, CircularProgress, Divider, Button } from '@material-ui/core';
+
+import useStyles from './styles';
+const steps=['Shipping address','Payment details'];
+
+const Checkout = () => {
+    const [activeStep, setActiveStep] = useState(0);
+    const classes=useStyles();
+
+    const Form=()=>activeStep ===0
+        ? <AddressForm/>
+        :<PaymentForm/>
+
+
+    return (
+        <AddressForm>
+            <div className={classes.toolbar}/>
+            <main className={classes.layout}>
+                <Paper className={classes.paper}>
+                    <Typography variant='h4' align='center'>Checkout</Typography>
+                    <Stepper activeStep={activeStep} className={classes.stepper}>
+                        {
+                            steps.map(step=>(
+                                <Step key={step}>
+                                    <StepLabel>{step}</StepLabel>
+                                </Step>
+                            ))
+                        }
+                    </Stepper>
+                        {activeStep ===steps.length ? <Confirmation/> :<Form/> }
+                </Paper>
+            </main>  
+        </AddressForm>
+    )
+}
+
+export default Checkout
